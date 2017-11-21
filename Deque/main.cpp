9f0b4baa874ccb122452b63c76270bcd717e7c9d
Hunaid Hassan <hhunaid@gmail.com>
2017-11-22 00:40:03 +0500

    Initial Commit

---
+++ Deque/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Deque
+//
+//  Created by Hunaid Hassan on 22/11/2017.
+//  Copyright © 2017 Hunaid Hassan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
